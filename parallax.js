b4057275449e0c406707f9482df1384d9fc87a5c
Aylor Brown <aylorbrown@gmail.com>
2020-04-01 21:16:03 -0400

    finishes parallax

--- parallax.js
+++ parallax.js
@@ -6,7 +6,7 @@ const addMovement = function () {
     const midViewport = topViewport + (window.innerHeight / 2)
 
     // middle of each section 
-    sections.forEach(section => {
+    sections.forEach((section, index) => {
         const topSection = section.offsetTop
         const midSection = topSection + (section.offsetHeight / 2)
 
@@ -17,8 +17,21 @@ const addMovement = function () {
         const image = section.querySelector("img")
         const content = section.querySelector("div")
 
+        // weight down this distance 
+        let rotation = distanceToSection / 100
+        let contentDistance = -1 * distanceToSection / 2
+
+        // for every even section, rotate the other way 
+        // is the index divisible by 2?
+        // use the modulo operator! 
+        if (index % 2 == 1) {
+            rotation = rotation * -1
+        }
+
         // apply some parallax
-        image.style.transform = `rotate(${distanceToSection}deg)`
+        image.style.transform = `rotate(${rotation}deg)`
+        content.style.top = `${contentDistance}px`
+        content.style.transform = `rotate(${-1 * rotation}deg)`
     })
 }
 
